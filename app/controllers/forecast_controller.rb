bb916fca37bb01891b34354fc01b4b5aa1d3d6cc
Spadari Sara <saraspadari@yahoo.com>
2026-02-18 15:44:08 +0100

    map background perfect

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,4 +1,18 @@
 class ForecastController < ApplicationController
   def index
   end
+
+  def kp_now
+    require 'net/http'
+    require 'json'
+
+    uri = URI('https://services.swpc.noaa.gov/json/planetary_k_index_1m.json')
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+
+    latest = data.last
+    kp = latest ? latest['kp_index'].round(1) : nil
+
+    render json: { kp: kp }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,13 @@
 Rails.application.routes.draw do
-  get 'notifications/index'
-  get 'weather/index'
-  get 'forecast/index'
-  get 'auroras/index'
-  get 'auroras/show'
-  get 'auroras/new'
-  get 'auroras/create'
-  get 'auroras/upload_photo'
-  get 'auroras/submit'
-  get 'sessions/set_geolocation'
-  get 'sessions/share_app'
-  get 'home/index'
   root 'home#index'
 
-  post '/geolocation',    to: 'sessions#set_geolocation'
+  post '/geolocation', to: 'sessions#set_geolocation'
 
-  resources :auroras, only: [:index, :show, :new, :create] do
-    member do
-      post :upload_photo
-      post :submit
-    end
-  end
+  resources :auroras, only: [:index, :show, :new, :create]
+
+  get '/weather',      to: 'weather#index'
+  get '/forecast',     to: 'forecast#index'
+  get '/forecast/kp_now', to: 'forecast#kp_now'
 
   resources :notifications, only: [:index]
-  resources :rewards,       only: [:index]
-  get '/forecast',          to: 'forecast#index'
-  get '/weather',           to: 'weather#index'
-  get '/share',             to: 'sessions#share_app'
 end
