ac5975dbefe47d1034dc42f6071d9fc047eb6140
Spadari Sara <saraspadari@yahoo.com>
2026-02-18 16:30:11 +0100

    added distance

--- app/controllers/auroras_controller.rb
+++ app/controllers/auroras_controller.rb
@@ -6,6 +6,13 @@ class AurorasController < ApplicationController
 def show
   @aurora = Aurora.find(params[:id])
   @minutes_ago = ((Time.current - @aurora.submitted_at) / 60).floor
+
+  if session[:latitude] && session[:longitude]
+    @distance_km = haversine_distance(
+      session[:latitude].to_f, session[:longitude].to_f,
+      @aurora.latitude, @aurora.longitude
+    )
+  end
 end
 
   def new
@@ -33,4 +40,14 @@ end
   def aurora_params
     params.require(:aurora).permit(:latitude, :longitude, :description, :photo)
   end
+
+  def haversine_distance(lat1, lng1, lat2, lng2)
+  rad = Math::PI / 180
+  dlat = (lat2 - lat1) * rad
+  dlng = (lng2 - lng1) * rad
+  a = Math.sin(dlat/2)**2 +
+      Math.cos(lat1 * rad) * Math.cos(lat2 * rad) * Math.sin(dlng/2)**2
+  (6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))).round(1)
+  end
+
 end
