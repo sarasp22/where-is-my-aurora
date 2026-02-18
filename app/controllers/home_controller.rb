e668190ca4239b6e92ea0cdfcefd4a159f5ff140
Spadari Sara <saraspadari@yahoo.com>
2026-02-18 16:08:53 +0100

    details popup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,13 @@ class HomeController < ApplicationController
   def index
     @auroras = Aurora.active.recent.limit(50)
     @auroras_json = @auroras.map do |a|
-      { lat: a.latitude, lng: a.longitude, id: a.id, description: a.description }
+      {
+        lat: a.latitude,
+        lng: a.longitude,
+        id: a.id,
+        description: a.description,
+        minutes_ago: ((Time.current - a.submitted_at) / 60).floor
+      }
     end.to_json
   end
 end
--- app/models/aurora.rb
+++ app/models/aurora.rb
@@ -5,5 +5,5 @@ class Aurora < ApplicationRecord
   has_one_attached :photo
 
   scope :recent, -> { order(submitted_at: :desc) }
-  scope :active, -> { where("submitted_at > ?", 15.minutes.ago) }
+  scope :active, -> { where("submitted_at > ?", 30.minutes.ago) }
 end
