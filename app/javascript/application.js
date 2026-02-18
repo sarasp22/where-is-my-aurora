88b4f688fd3e9ed4eec38ea8de6784746e7cb431
Spadari Sara <saraspadari@yahoo.com>
2026-02-18 17:00:34 +0100

    test map

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,3 @@
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
 import "controllers"
