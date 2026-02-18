7bc6f5a80de92715fd6d7ac2e0e1cbbd0c03e492
Spadari Sara <saraspadari@yahoo.com>
2026-02-18 15:46:07 +0100

    kp works perfectly

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -6,13 +6,20 @@ class ForecastController < ApplicationController
     require 'net/http'
     require 'json'
 
-    uri = URI('https://services.swpc.noaa.gov/json/planetary_k_index_1m.json')
-    response = Net::HTTP.get(uri)
-    data = JSON.parse(response)
+    begin
+      uri = URI('https://services.swpc.noaa.gov/json/planetary_k_index_1m.json')
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri)
+      response = http.request(request)
+      data = JSON.parse(response.body)
 
-    latest = data.last
-    kp = latest ? latest['kp_index'].round(1) : nil
+      latest = data.last
+      kp = latest ? latest['kp_index'].round(1) : nil
 
-    render json: { kp: kp }
+      render json: { kp: kp }
+    rescue => e
+      render json: { kp: nil, error: e.message }
+    end
   end
 end
